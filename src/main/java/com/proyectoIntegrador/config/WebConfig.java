d677ef46144d4b0752c93cf9f6f20eadf2d6b6a6
Sebkill112 <snavarrosam45@gmail.com>
2023-11-16 09:40:22 -0500

    cambio origins

--- src/main/java/com/proyectoIntegrador/config/WebConfig.java
+++ src/main/java/com/proyectoIntegrador/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000")
+        .allowedOrigins("https://benevolent-pegasus-c14585.netlify.app/")
         .allowedMethods("GET", "POST", "PUT", "DELETE")
         .allowedHeaders("*");
     }
